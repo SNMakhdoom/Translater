961f05605f91be156793f97fc3af9e0cd08525d3
SNMakhdoom <ahsanmakhdoom@hotmail.com>
2023-08-16 13:21:56 +0500

    commit

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 from flask import Flask, request, jsonify, send_from_directory
 import subprocess
 import whisper
-from pdf2image import convert_from_path
-import pytesseract
+import pyaudio
+import wave
+import tempfile
 import os
+import pytesseract
+from pdf2image import convert_from_path
 
 app = Flask(__name__)
 model = whisper.load_model("base")
@@ -15,19 +18,42 @@ def index():
 @app.route('/transcribe', methods=['POST'])
 def transcribe_audio():
     try:
+        CHUNK = 1024
+        FORMAT = pyaudio.paInt16
+        CHANNELS = 1
+        RATE = 44100
+        RECORD_SECONDS = 600
+
+        audio_dir = '/Users/ahsan/Downloads/project_directory/recordings'
+        if not os.path.exists(audio_dir):
+            os.makedirs(audio_dir)
+
         audio_file = request.files['audio']
-        audio_path = 'Dr_Muhmad_ali.m4a'
-        audio_file.save(audio_path)
-        
-        audio_out = 'audio_output.mp3'
-        ffmpeg_cmd = f"/opt/homebrew/bin/ffmpeg -y -i {audio_path} -vn -c:a libmp3lame -b:a 192k {audio_out}"
+        audio_wav_path = os.path.join(audio_dir, 'temp_audio.wav')
+        audio_file.save(audio_wav_path)
+
+        audio_mp3_path = os.path.join(audio_dir, 'output_audio.mp3')
+
+        ffmpeg_cmd = f"/opt/homebrew/bin/ffmpeg -y -i {audio_wav_path} -vn -c:a libmp3lame -b:a 192k {audio_mp3_path}"
         subprocess.run(ffmpeg_cmd, shell=True)
-                
-        result = model.transcribe(audio_out)
-        
-        return jsonify({'transcription': result['text']})
+        print(f"Converted to MP3: {audio_mp3_path}")
+
+        result = model.transcribe(audio_mp3_path)
+    
+        # Optionally delete temp WAV file
+        os.remove(audio_wav_path)
+
+        audio_mp3_url = f"/get_audio/{os.path.basename(audio_mp3_path)}"
+
+        return jsonify({'transcription': result['text'], 'audio_url': audio_mp3_url})
     except Exception as e:
+        print(f"An error occurred: {str(e)}")
         return jsonify({'error': str(e)}), 400
+
+@app.route('/get_audio/<filename>')
+def get_audio(filename):
+    audio_dir = '/Users/ahsan/Downloads/project_directory/recordings'
+    return send_from_directory(audio_dir, filename)
 
 @app.route('/pdf_to_text', methods=['POST'])
 def pdf_to_text():
